e77dc49cad5011cde24ae08f98f5d62c72a603cc
Matt <mattpuchalski5@gmail.com>
2023-08-06 15:08:49 -0400

    PyPI Upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CyberCells",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'CyberCells=CyberCells:main',
+        ],
+    },
+    install_requires=[
+        'numpy',
+    ],
+    author="Matt Puchalski",
+    author_email="me@mattp.tech",
+    description="A simple Cyberpunk inspired Sudoku game",
+    keywords="python Cyberpunk sudoku game",
+    url="https://github.com/lasermatts/CyberCells",
+)
